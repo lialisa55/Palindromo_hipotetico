8cd411787993e689fb1908bd8d679af0fbb66ba9
lialisa55 <nathaliamonalisa150@gmail.com>
2024-05-07 00:44:03 -0300

    programa completo

--- main.c
+++ main.c
@@ -3,16 +3,48 @@
 
 int main()
 {
-    int n, *p;
-    scanf("%d", &n);
-    int vetor[n];
-
-    for (p = vetor; p < vetor+n; p++){
-        scanf("%c", p);
+    long int n, palindromo = 1;
+    scanf("%ld", &n);
+    char vetor[n], *p1 = vetor, *p2 = vetor+n-1;
+    
+    for (int i = 0; i < n; i++){
+	    scanf(" %c", &vetor[i]);
+    }
+    
+    /*while (p1 < p2){
+	    if(*p1 != *p2){
+		    palindromo = 0;
+		    break;
+	    }
+	    p1++;
+	    p2--;
     }
 
-    for (p = vetor + n - 1; p >= vetor; --p){
-        printf("%c", *p);
+    if(palindromo){
+	    printf("Sim\n");
+    }
+    else{
+	    printf("Nao\n");
+    }*/
+	
+    int contador[26], impares = 0;
+
+    for (int i = 0; i < n; i++){
+	    contador[vetor[i] - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++){
+	    if (contador[i] % 2 != 0) {
+		    impares++;
+	    }
+    }
+
+    if (impares <= 1){
+	    printf("Sim");
+    }
+    else {
+	    printf("Nao");
     }
     return 0;
+
 }
